9018dab78e273a60910161da675b16f18947f89e
Mikhail Kuzmin <revan0057@gmail.com>
2020-05-11 10:22:18 +0300

    upd

--- Deque/is_palindrome.py
+++ Deque/is_palindrome.py
@@ -4,15 +4,13 @@ from Deque import Deque
 # check string is palindrome (ignore punctuation)
 def is_palindrome(string: str):
     deque = Deque()
-    processed_str = ''
-    reversed_str = ''
 
     for s in string:
         if s.lower().isalpha():
             deque.addTail(s)
-            processed_str += s
 
     while deque.size() > 0:
-        reversed_str += deque.removeTail()
+        if deque.removeFront() != deque.removeTail():
+            return False
 
-    return processed_str == reversed_str
+    return True
